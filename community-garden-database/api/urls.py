3d9bc791904df69ef54561f82c826a69faea74a6
Juan Andres <Juan@Devins-MacBook-Air.local>
2022-03-27 01:44:51 -0400

    Added Change Password feature

--- community-garden-database/api/urls.py
+++ community-garden-database/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views.user import SignUp, SignIn, SignOut
+from .views.user import SignUp, SignIn, SignOut, ChangePassword
 
 urlpatterns = [
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
+    path('change-password/', ChangePassword.as_view(), name='change-password'),
+
 ]
